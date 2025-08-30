698714b9d00e5a2a18eabccfb69fb237b00c88ba
CrazyVillager <makadem11@web.de>
2025-08-30 15:02:31 +0200

    now the skin selection works; added the two last main menu backgrounds

--- Assets/GameFiles/Scripts/System/UI/SkinManager.cs
+++ Assets/GameFiles/Scripts/System/UI/SkinManager.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -28,14 +32,11 @@ public class SkinManager : Singleton<SkinManager>, IDataPersistence
 	}
 
 	public void LoadData(GameData data)
-    {
-        skins = data.skins;
-        foreach (Skin skin in skins)
-        {
-	        if (skin.isCollected)
-		        SelectSkin(skin);
-        }
-    }
+	{
+		skins = data.skins;
+		var skin = skins.Find(s => s.name == "StandardSkin");
+		SelectSkin(skin);
+	}
 
     public void SaveData(ref GameData data)
     {
@@ -53,21 +54,26 @@ public class SkinManager : Singleton<SkinManager>, IDataPersistence
 		SelectSkin(skin);
     }
 
-    public void SelectSkin(Skin skin)
+    public async void SelectSkin(Skin skin)
     {
+	    var isMainMenu =  await WaitForMainMenu();
         skin.color.a = 1; // not 255 because these values represent the percentages of each color part (1 is the maximum)
         playerPrefab.GetComponent<SpriteRenderer>().color = skin.color;
 
         Skin selectedSkinObj = skins.Find(s => s.isSelected);
         if (selectedSkinObj != null)
         {
-	        GameObject.Find(selectedSkinObj.name).GetComponent<Image>().sprite = normalSkin;
+	        var skinObj = GameObject.Find(selectedSkinObj.name);	
+	        if (skinObj != null) skinObj.GetComponent<Image>().sprite = normalSkin;
 	        selectedSkinObj.isSelected = false;
+	        
+	        var skinobj = GameObject.Find(skin.name); 
+	        skin.isSelected = true;
+	        if(skinobj != null)
+		        skinobj.GetComponent<Image>().sprite = selectedSkin;
+	        else print("something went wrong SkinManager Ln 73");
         }
-
-        GameObject currentSkin = GameObject.Find(skin.name);
-        currentSkin.GetComponent<Image>().sprite = selectedSkin;
-        skin.isSelected = true;
+		else Debug.LogWarning("Selected skin not found (Line 70 SkinManager.cs)");
         
         print($"skin got selected with (R: {skin.color.r}, G: {skin.color.g}, B: {skin.color.b}, A: {skin.color.a}) color");
     }
@@ -81,22 +87,43 @@ public class SkinManager : Singleton<SkinManager>, IDataPersistence
 
 		foreach(Skin skin in skins)
 		{
-			if(!skin.isCollected && skin.name != "StandardSkin")
+			GameObject skinObj = GameObject.Find(skin.name);
+			if(!skin.isCollected)
 			{
 				print(skin.name);
-				GameObject skinObj = GameObject.Find(skin.name);
 				skinObj.GetComponent<Image>().sprite = lockedSkin;
 				skinObj.transform.GetChild(0).gameObject.SetActive(true);
+			}
+
+			if (skin.isCollected)
+			{
+				print($"{skin.name} is already collected");
+				
+				if (skin.isSelected)
+					skinObj.GetComponent<Image>().sprite = selectedSkin;
+				else
+					skinObj.GetComponent<Image>().sprite = normalSkin;
+				
+				if(skinObj.name != "StandardSkin")
+					skinObj.transform.GetChild(0).gameObject.SetActive(false);
 			}
 			else
 			{
 				if (skin.name == "StandardSkin")
-				{
 					print($"Skip standard skin");
-				}
 				else
 					print($"no skin with name {skin.name} found!");
 			}
 		}
+	}
+
+	private async Task<bool> WaitForMainMenu()
+	{
+		while (GameManager.Instance.gameState != GameState.MainMenu)
+		{
+			await Task.Yield();
+		}
+
+		return true;
 	}
 }
