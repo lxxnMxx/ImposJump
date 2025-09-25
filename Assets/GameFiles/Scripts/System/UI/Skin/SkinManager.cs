23869d5cf4c766c28ae9c58935b40d303feb455a
CrazyVillager <makadem11@web.de>
2025-09-25 08:45:37 +0200

    Build ready version (Build-2025-09-25 Beta 1_2)

--- Assets/GameFiles/Scripts/System/UI/Skin/SkinManager.cs
+++ Assets/GameFiles/Scripts/System/UI/Skin/SkinManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FirstGearGames.SmoothCameraShaker;
-using Unity.Scenes.Editor;
 using UnityEngine;
 
 public class SkinManager : Singleton<SkinManager>, IDataPersistence
